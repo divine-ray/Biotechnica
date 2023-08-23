7f97fb0ef5336e390f604f8bea3f4d360e29a67d
CreepTV <lixernispunkt2@gmx.net>
2023-08-23 23:26:38 +0200

    add gradle files more pain

--- src/main/java/net/divineray/biotech/items/greenBacterium.java
+++ src/main/java/net/divineray/biotech/items/greenBacterium.java
@@ -5,7 +5,6 @@ import java.util.List;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.EnumChatFormatting;
 
 public class greenBacterium extends Item {
 
@@ -14,6 +13,6 @@ public class greenBacterium extends Item {
     @Override
     @SuppressWarnings("rawtypes")
     public void addInformation(ItemStack itemstack, EntityPlayer player, List tooltip, boolean bool) {
-        tooltip.add(EnumChatFormatting.GREEN + "A green-coloured bacterium. Lethal.");
+        tooltip.add(net.minecraft.util.EnumChatFormatting.GREEN + "A green-coloured bacterium. Lethal.");
     }
 }
